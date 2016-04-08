130c55c246b370093eb5a22f58753a1d982d4d81
Stuart Olivera <stuart@stuartolivera.com>
2016-04-08 11:55:52 -0400

    Fixed private key for travis deployment

--- .travis/deploy_prod.sh
+++ .travis/deploy_prod.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)" # start the ssh agent
-openssl aes-256-cbc -K $encrypted_892a6c2690c6_key -iv $encrypted_892a6c2690c6_iv -in marketing_rsa.enc -out marketing_rsa -d
+openssl aes-256-cbc -K $encrypted_892a6c2690c6_key -iv $encrypted_892a6c2690c6_iv -in .travis/marketing_rsa.enc -out .travis/marketing_rsa -d
 chmod 600 .travis/marketing_rsa # this key should have push access
 ssh-add .travis/marketing_rsa
 git remote add deploy dokku@csh-cloud.oweb.co:herrd-push-server
